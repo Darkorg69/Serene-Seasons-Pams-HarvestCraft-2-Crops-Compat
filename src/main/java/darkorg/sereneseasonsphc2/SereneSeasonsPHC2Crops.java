9ccfa6841a807f282e1193dd61f9964f20da3b32
Darkorg69 <k.mikaelson78@gmail.com>
2022-11-29 22:01:20 +0200

    Initial commit

---
+++ src/main/java/darkorg/sereneseasonsphc2/SereneSeasonsPHC2Crops.java
@@ -0,0 +1,12 @@
+package darkorg.sereneseasonsphc2;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("sereneseasonsphc2crops")
+
+public class SereneSeasonsPHC2Crops {
+    public SereneSeasonsPHC2Crops() {
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+}
