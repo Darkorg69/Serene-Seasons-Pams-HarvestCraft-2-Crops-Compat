5a7337d744170f154bf50f86f9c95c734c890e89
Hristo Gospodinov <k.mikaelson78@gmail.com>
2024-02-02 02:42:16 +0200

    Update 1.19.2-1.0.1
    -Update Forge to 43.3.7
    
    -Port changes from 1.18.2-1.0.2

--- src/main/java/darkorg/sereneseasonsphc2crops/SereneSeasonsPHC2Crops.java
+++ src/main/java/darkorg/sereneseasonsphc2crops/SereneSeasonsPHC2Crops.java
@@ -4,8 +4,9 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod("sereneseasonsphc2crops")
-
 public class SereneSeasonsPHC2Crops {
+    public static final String MOD_ID = "sereneseasonsphc2crops";
+
     public SereneSeasonsPHC2Crops() {
         MinecraftForge.EVENT_BUS.register(this);
     }
---
+++ src/main/java/darkorg/sereneseasonsphc2crops/event/ModEvents.java
@@ -0,0 +1,25 @@
+package darkorg.sereneseasonsphc2crops.event;
+
+import darkorg.sereneseasonsphc2crops.SereneSeasonsPHC2Crops;
+import darkorg.sereneseasonsphc2crops.data.server.ModBlockTagsProvider;
+import darkorg.sereneseasonsphc2crops.data.server.ModItemTagsProvider;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.data.event.GatherDataEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = SereneSeasonsPHC2Crops.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModEvents {
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event) {
+        DataGenerator generator = event.getGenerator();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+
+        ModBlockTagsProvider blockTagsProvider = new ModBlockTagsProvider(generator, existingFileHelper);
+        ModItemTagsProvider itemTagsProvider = new ModItemTagsProvider(generator, blockTagsProvider, existingFileHelper);
+
+        generator.addProvider(event.includeServer(), blockTagsProvider);
+        generator.addProvider(event.includeServer(), itemTagsProvider);
+    }
+}
