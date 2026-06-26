a43477d39c085cf18b5106d5ad439202bda24f3e
steffyjones69 <stefka.gospodinova@icloud.com>
2026-06-26 13:10:42 +0300

    1.21.1-0.6.9
    - commented out forge dependencies
    - updated dependencies to match the game version
    
    to fix:
    - bone meal makes pam's crops grow outside of fertile season
    - attempts to save and exit the world fail without a crash, but after killing minecraft and starting it again, it turns out that the progress is successfully saved

--- src/main/java/darkorg/sereneseasonsphc2crops/SereneSeasonsPHC2Crops.java
+++ src/main/java/darkorg/sereneseasonsphc2crops/SereneSeasonsPHC2Crops.java
@@ -2,16 +2,11 @@ package darkorg.sereneseasonsphc2crops;
 
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.neoforge.common.NeoForge;
-import org.slf4j.Logger;
-import com.mojang.logging.LogUtils;
 
 @Mod("sereneseasonsphc2crops")
 public class SereneSeasonsPHC2Crops {
     public static final String MOD_ID = "sereneseasonsphc2crops";
-    private static final Logger LOGGER = LogUtils.getLogger();
 
     public SereneSeasonsPHC2Crops(IEventBus modEventBus) {
-        NeoForge.EVENT_BUS.register(this);
     }
 }
