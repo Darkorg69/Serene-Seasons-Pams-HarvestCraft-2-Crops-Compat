2189babe9b76dfccbd9cf2ee3ded563192f57749
steffyjones69 <stefka.gospodinova@icloud.com>
2026-06-26 04:26:08 +0300

    1.21-0.6.9
    - commented out all forge dependencies as pam crops is available only on neoforge for minecraft 1.21
    - updated neoforge, pam crops, serene seasons and glitch dependencies
    - tested the jar file
    
    to fix:
    - bone meal still makes pam's crops grow outside of fertile seasons
    - the game bugs without a crash when you try to save and exit the world, however the progress is saved properly

--- NeoForge/src/main/java/darkorg/sereneseasonsphc2crops/neoforge/event/ModEvents.java
+++ NeoForge/src/main/java/darkorg/sereneseasonsphc2crops/neoforge/event/ModEvents.java
@@ -7,13 +7,13 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 
 import java.util.concurrent.CompletableFuture;
 
-@Mod.EventBusSubscriber(modid = SereneSeasonsPHC2CropsCommon.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+@EventBusSubscriber(modid = SereneSeasonsPHC2CropsCommon.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
 public class ModEvents {
     @SubscribeEvent
     public static void gatherData(GatherDataEvent event) {
